ca06b522efd3641ab694c068b3cb2518d4eaca18
Simon Duchesneau <s.duchesneau@hotmail.com>
2022-07-15 15:40:09 -0400

    Adding Hello World

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+    for i in range(1, 6):
+        print(f'Hello World {i}!')
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-
-
