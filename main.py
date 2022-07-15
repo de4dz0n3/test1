8cbbb45f2ed4e68d2b872aada8746b1589ca0c32
Simon Duchesneau <s.duchesneau@hotmail.com>
2022-07-15 15:59:31 -0400

    Update

--- main.py
+++ main.py
@@ -1,10 +1,39 @@
-def print_hi(name):
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import glob
+import os
+import requests
+import sys
 
+
+def main():
+    # print_hello_world()
+    # print_hi('Simon')
+    # read('C:\\WINDOWS\\DirectX.log')
+    get('https://www.google.ca/')
+
+
+def get(url):
+    response = requests.get(url)
+    print(response.status_code)
+    print(response.content)
+
+
+def read(file):
+    stream = open(file)
+    string = stream.read()
+    stream.close()
+
+    print(string)
+
+
+def print_hello_world():
     for i in range(1, 6):
         print(f'Hello World {i}!')
 
 
+def print_hi(name):
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
